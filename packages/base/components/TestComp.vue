49ce422a63751bb5609d9d09543744d347640f06
Roman Vašek <rvasek0@gmail.com>
2023-02-28 20:06:04 +0100

    init

---
+++ packages/base/components/TestComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <img src="~/assets/img/test.png" />
+  <div id="asset-in-css"></div>
+</template>
+
+<style scoped>
+img,
+#asset-in-css {
+  width: 512px;
+  height: 512px;
+}
+
+#asset-in-css {
+  background: url("@/assets/img/test.png") no-repeat;
+}
+</style>
